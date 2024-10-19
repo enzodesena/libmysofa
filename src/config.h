5f9f834302e6147da17c74a7ffbdcea080c41f89
Enzo De Sena <3883487+enzodesena@users.noreply.github.com>
2024-10-19 13:22:25 +0100

    Hard adding config.h and mysofa_export.h (presumably only works on the machine it was generated on with cmake, MACOS in this case)

---
+++ src/config.h
@@ -0,0 +1,10 @@
+
+#if !defined _CONFIG_H
+#define _CONFIG_H
+
+#define CMAKE_INSTALL_PREFIX "/Users/enzodesena/repos_others/libmysofa/build/delete"
+#define CPACK_PACKAGE_VERSION_MAJOR 1
+#define CPACK_PACKAGE_VERSION_MINOR 3
+#define CPACK_PACKAGE_VERSION_PATCH 2
+
+#endif
